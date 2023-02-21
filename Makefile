eadc3db4cf0d28509241687cf09a913522e57cab
Merhawi Kiflemariam <mkiflema@lab1r4s2.42abudhabi.ae>
2023-02-21 13:41:31 +0400

    this

---
+++ Makefile
@@ -0,0 +1,43 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: mkiflema <mkiflema@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/02/10 20:21:25 by mkiflema          #+#    #+#              #
+#    Updated: 2023/02/20 22:40:58 by mkiflema         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME	= pipex
+
+M_SRCS	= ft_pipex.c ft_utils.c main.c
+B_SRCS	= ft_pipex_bonus.c constr_pipex_bonus.c ft_utils_bonus.c ft_here_doc.c main_bonus.c
+
+CFLAGS	= -Wall -Wextra -Werror
+CC 		= cc
+RM		= rm -f
+
+M_OBJS	= $(M_SRCS:.c=.o)
+B_OBJS	= $(B_SRCS:.c=.o)
+
+all: $(NAME)
+
+$(NAME): $(M_OBJS)
+	@cd ./libft && make
+	@$(CC) $(CFLAGS) $(M_OBJS) ./libft/libft.a -o $(NAME)
+
+clean:
+	@cd ./libft && make clean
+	@$(RM) $(M_OBJS) $(B_OBJS)
+
+fclean: clean
+	@cd ./libft && make fclean
+	@$(RM) $(NAME)
+	
+bonus: $(B_OBJS)
+	@cd ./libft && make
+	@$(CC) $(CFLAGS) $(B_OBJS) ./libft/libft.a -o $(NAME)
+
+re: fclean all
---
+++ ft_pipex_bonus.c
@@ -0,0 +1,122 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pipex_bonus.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkiflema <mkiflema@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/17 14:13:24 by mkiflema          #+#    #+#             */
+/*   Updated: 2023/02/21 12:33:19 by mkiflema         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_pipex_bonus.h"
+
+static void	ft_last_child_proc(int *fd[], char **argv, char *path, int len)
+{
+	int	fd2;
+
+	if (get_pid() == 0)
+	{
+		fd2 = open(argv[len + 3], O_CREAT | O_WRONLY | O_TRUNC, 0777);
+		dup2(fd[len - 1][0], STDIN_FILENO);
+		dup2(fd2, STDOUT_FILENO);
+		close(fd2);
+		close_pipes(fd, len);
+		if (!ft_get_paths(path, argv[len + 2]))
+		{
+			ft_putstr_fd("cmd not la found\n", 2);
+			exit(4);
+		}
+		if (execve(ft_get_paths(path, argv[len + 2]),
+				ft_split(argv[len + 2], ' '), NULL) == -1)
+		{
+			perror("execve error");
+			exit(8);
+		}
+		exit(9);
+	}
+}
+
+static void	ft_child_pro_middle(int *fd[], char **argv, char *path, int len)
+{
+	int	i;
+
+	i = 1;
+	while (i < len)
+	{
+		if (get_pid() == 0)
+		{
+			dup2(fd[i - 1][0], 0);
+			dup2(fd[i][1], 1);
+			close_pipes(fd, len);
+			if (!ft_get_paths(path, argv[i + 2]))
+			{
+				ft_putstr_fd("cmd not's foun\n", 2);
+				exit(2);
+			}
+			if (execve(ft_get_paths(path, argv[i + 2]),
+					ft_split(argv[i + 2], ' '), NULL) == -1)
+			{
+				perror("execve");
+				exit(8);
+			}
+			exit(4);
+		}
+		i++;
+	}
+}
+
+static void	ft_child_pro_one(int *fd[], char **argv, char *path, int len)
+{
+	int	fd1;
+
+	if (get_pid() == 0)
+	{
+		fd1 = open(argv[1], O_RDONLY, 0777);
+		dup2(fd1, STDIN_FILENO);
+		dup2(fd[0][1], STDOUT_FILENO);
+		if (!ft_get_paths(path, argv[2]))
+		{
+			ft_putstr_fd("cmd not foun\n", 2);
+			exit(2);
+		}
+		close(fd1);
+		close_pipes(fd, len);
+		if (execve(ft_get_paths(path, argv[2]),
+				ft_split(argv[2], ' '), NULL) == -1)
+		{
+			perror("execve");
+			exit(8);
+		}
+		exit(4);
+	}
+}
+
+void	ft_pipex_bonus(char **argv, char *envp[], int len)
+{
+	int	**fd;
+	int	i;
+
+	fd = (int **)malloc(len * sizeof(int *));
+	if (!fd)
+		return ;
+	i = 0;
+	while (i < len)
+		fd[i++] = (int *)malloc(2 * sizeof(int));
+	ft_creat_pipe(len, fd);
+	i = ft_search_path(envp, "PATH=");
+	if (ft_strncmp(argv[1], "here_doc", ft_strlen(argv[1])) == 0)
+		argv = here_doc(fd, argv, envp[i], len);
+	else
+		ft_child_pro_one(fd, argv, envp[i], len);
+	if (len > 1)
+	{
+		ft_child_pro_middle(fd, argv, envp[i], len);
+	}
+	ft_last_child_proc(fd, argv, envp[i], len);
+	close_pipes(fd, len);
+	i = -1;
+	while (++i < (len + 1))
+		wait(NULL);
+}
